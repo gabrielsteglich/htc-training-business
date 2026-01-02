802b58cb7931275936e3f343b23325bdb56724fb
gabrielsteglich <63128571+gabrielsteglich@users.noreply.github.com>
2026-01-02 00:28:42 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: process.env.VITE_BASE_URL || '/',
+  base: '/htc-training-business/',
   server: {
     port: 3000,
     host: '0.0.0.0',
